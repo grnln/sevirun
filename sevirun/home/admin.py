4a6c6f872d3762df7f55e7e92c77189e4294de78
mquirosq <mquirosquiroga@gmail.com>
2025-11-05 20:47:24 +0100

    Improve home page

--- sevirun/home/admin.py
+++ sevirun/home/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import DisplayCase, Article
+from .models import Product
 
-admin.site.register(DisplayCase)
-admin.site.register(Article)
+admin.site.register(Product)
--- sevirun/home/models.py
+++ sevirun/home/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 
-class Article(models.Model):
+class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to='articles/')
+    image = models.ImageField(upload_to='products/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
-
-class DisplayCase(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return str(self.article.id)
