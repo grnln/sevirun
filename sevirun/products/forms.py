1e8564d9ea2fbd4b0977300aec73930e7af9d66f
Guillermo Rodríguez Narbona <guillermo.rn04@gmail.com>
2025-11-07 19:00:58 +0100

    Implemented filters in the product listing screen #3.

--- sevirun/products/forms.py
+++ sevirun/products/forms.py
@@ -1,8 +1,44 @@
 from django import forms
 from .models import *
 
+def get_brand_choices():
+    out = [('None', 'Todas')]
+    out.extend([(str(b.pk), b.name) for b in Brand.objects.all()])
+    return out
+
 def get_product_type_choices():
-    return [(str(t.pk), t.name) for t in ProductType.objects.all()]
+    out = [('None', 'Todos')]
+    out.extend([(str(t.pk), t.name) for t in ProductType.objects.all()])
+    return out
+
+def get_product_model_choices():
+    return [('None', 'Seleccione una marca')]
+
+def get_product_season_choices():
+    out = [('None', 'Todas')]
+    out.extend([(str(s.pk), s.name) for s in ProductSeason.objects.all()])
+    return out
+
+def get_product_material_choices():
+    out = [('None', 'Todos')]
+    out.extend([(str(m.pk), m.name) for m in ProductMaterial.objects.all()])
+    return out
+
+def get_product_size_choices():
+    out = [('None', 'Todas')]
+    out.extend([(str(s.pk), s.name) for s in ProductSize.objects.all()])
+    return out
+
+def get_product_colour_choices():
+    out = [('None', 'Todos')]
+    out.extend([(str(c.pk), c.name) for c in ProductColour.objects.all()])
+    return out
 
 class ProductFiltersForm(forms.Form):
+    brand = forms.ChoiceField(required = False, label = 'Marca', choices = get_brand_choices)    
     type = forms.ChoiceField(required = False, label = 'Tipo', choices = get_product_type_choices)
+    model = forms.ChoiceField(required = False, label = 'Modelo', choices = get_product_model_choices)
+    season = forms.ChoiceField(required = False, label = 'Temporada', choices = get_product_season_choices)
+    material = forms.ChoiceField(required = False, label = 'Material', choices = get_product_material_choices)
+    size = forms.ChoiceField(required = False, label = 'Talla', choices = get_product_size_choices)
+    colour = forms.ChoiceField(required = False, label = 'Color', choices = get_product_colour_choices)
--- sevirun/products/views.py
+++ sevirun/products/views.py
@@ -4,12 +4,48 @@ from django.http import HttpResponseRedirect
 from .models import Product
 from .forms import *
 
+def get_product_model_choices_with_brand(brand):
+    out = [("None", "Todos")]
+    out.extend([(str(m.pk), m.name) for m in ProductModel.objects.all().filter(brand = brand)])
+    return out
+
 def index(request):
     products = Product.objects.all()
 
     if request.method == 'POST':
         filters = ProductFiltersForm(request.POST)
-        products = products.filter(type = request.POST.get('type'))
+
+        brand_filter = request.POST.get('brand')
+        type_filter = request.POST.get('type')
+        model_filter = request.POST.get('model')
+        season_filter = request.POST.get('season')
+        material_filter = request.POST.get('material')
+        size_filter = request.POST.get('size')
+        colour_filter = request.POST.get('colour')
+
+        if brand_filter.lower() != 'none':
+            filters.fields['model'].choices = get_product_model_choices_with_brand(brand_filter)
+            products = products.filter()
+
+        if type_filter.lower() != 'none':
+            products = products.filter(type = type_filter)
+        
+        if model_filter.lower() != 'none':
+            products = products.filter(model = model_filter)
+
+        if season_filter.lower() != 'none':
+            products = products.filter(season = season_filter)
+
+        if material_filter.lower() != 'none':
+            products = products.filter(material = material_filter)
+
+        if size_filter.lower() != 'none':
+            products_with_size = [s.product for s in ProductStock.objects.all().filter(size = size_filter)]
+            products = [p for p in products if p in products_with_size]
+
+        if colour_filter.lower() != 'none':
+            products_with_colour = [s.product for s in ProductStock.objects.all().filter(colour = colour_filter)]
+            products = [p for p in products if p in products_with_colour]
     else:
         filters = ProductFiltersForm()
 
