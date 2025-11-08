29b8860d3289ff9ec08b5f4e03a5d628b9d07660
IMora04 <ignmorper1@alum.us.es>
2025-11-08 18:12:34 +0100

    feat: added admin sale listing screen. Added sales button to navbar
    #31

--- sevirun/orders/views.py
+++ sevirun/orders/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import *
-from django.http import HttpResponse
+from django.contrib.admin.views.decorators import staff_member_required
 
 # Create your views here.
+
+@staff_member_required
+def index_sales(request):
+    sales = Order.objects.filter(state="DE")
+    return render(request, 'sales_list.html', { "sales" : sales })
