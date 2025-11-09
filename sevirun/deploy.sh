8a0d033a6b83a2580cd2ba2482ec40a89f0e2a22
IMora04 <ignmorper1@alum.us.es>
2025-11-09 11:45:17 +0100

    build: removed running command from build script

--- sevirun/deploy.sh
+++ sevirun/deploy.sh
@@ -4,5 +4,4 @@ python manage.py migrate --noinput && \
 python manage.py collectstatic --noinput && \
 python manage.py loaddata users/fixtures/sample.json && \
 python manage.py loaddata products/fixtures/sample.json && \
-python manage.py loaddata orders/fixtures/sample.json && \
-gunicorn sevirun.wsgi:application
+python manage.py loaddata orders/fixtures/sample.json
