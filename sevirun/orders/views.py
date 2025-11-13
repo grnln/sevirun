fc9c598a705cea9bedcf57924d1652b89461444d
Guillermo Rodríguez Narbona <guillermo.rn04@gmail.com>
2025-11-13 14:22:19 +0100

    Fixed broken orders tests.

--- sevirun/orders/views.py
+++ sevirun/orders/views.py
@@ -15,7 +15,7 @@ def index_customer_orders(request):
     if request.user.is_staff or request.user.is_superuser:
         return redirect('sales')
     orders = Order.objects.filter(client=request.user).order_by('-created_at')
-    return render(request, 'orders_list.html', { "orders" : orders })
+    return render(request, 'orders/orders_list.html', { "orders" : orders })
 
 @login_required(login_url='login')
 def order_detail(request, order_id):
@@ -23,11 +23,11 @@ def order_detail(request, order_id):
         order = Order.objects.get(id=order_id)
     except Order.DoesNotExist:
         messages.error(request, "El pedido no existe.")
-        return redirect('index_customer_orders')
+        return redirect('orders/index_customer_orders')
 
     if not (order.client == request.user):
         messages.error(request, "No tienes permiso para ver este pedido.")
-        return redirect('index_customer_orders')
+        return redirect('orders/index_customer_orders')
 
     order_items = OrderItem.objects.filter(order=order)
-    return render(request, 'order_detail.html', { "order": order, "order_items": order_items })
+    return render(request, 'orders/order_detail.html', { "order": order, "order_items": order_items })
