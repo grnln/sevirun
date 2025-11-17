4b96feb0c7861fb8cf0ced61a3cbac12239170f1
Guillermo Rodríguez Narbona <guillermo.rn04@gmail.com>
2025-11-17 22:57:49 +0100

    Added the product search to the home page.

--- sevirun/home/views.py
+++ sevirun/home/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from products.models import Product
 from django.http import HttpResponse
 
+from products.forms import ProductFiltersForm
+
 def home(request):
     products = Product.objects.all().filter(is_highlighted=True)
     context = {
         'products': products,
+        'filters': ProductFiltersForm()
     }
     return render(request, 'home/home.html', context)
 
